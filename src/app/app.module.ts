95745b5736d623e654018ca26ee166c61571da44
macem <marcin.soltysiuk@gmail.com>
2019-02-28 22:45:07 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {Routes, RouterModule, Router, CanActivate, CanActivateChild} from '@angular/router';
+import { Routes, RouterModule, Router, CanActivate, CanActivateChild } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,21 +13,22 @@ import { ContactComponent } from './contact/contact.component';
 import { UserComponent } from './user/user.component';
 import { ChangeInfoComponent } from './change-info/change-info.component';
 import { AddDeletePlanComponent } from './add-delete-plan/add-delete-plan.component';
-import {AuthenticationGuardGuard} from './authentication-guard.guard';
+import { AuthenticationGuardGuard } from './authentication-guard.guard';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'index', pathMatch: 'full'},
-  {path: 'index', component: AppIndexComponent},
-  {path: 'signup', component: SignupComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'contact', component: ContactComponent},
-  {path:  'user', component: UserComponent, children: [
-    {path: 'change_info', component: ChangeInfoComponent},
-    {path: 'plan', component: AddDeletePlanComponent}
+  { path: '', redirectTo: 'index', pathMatch: 'full' },
+  { path: 'index', component: AppIndexComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'contact', component: ContactComponent },
+  { path:  'user', component: UserComponent, children: [
+    { path: 'change_info', component: ChangeInfoComponent },
+    { path: 'plan', component: AddDeletePlanComponent },
   ],
-canActivate: [AuthenticationGuardGuard],
-canActivateChild: [AuthenticationGuardGuard]}
+  canActivate: [AuthenticationGuardGuard],
+  canActivateChild: [AuthenticationGuardGuard]},
 ];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -37,16 +38,17 @@ canActivateChild: [AuthenticationGuardGuard]}
     ContactComponent,
     UserComponent,
     ChangeInfoComponent,
-    AddDeletePlanComponent
+    AddDeletePlanComponent,
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
     ReactiveFormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
+
 export class AppModule { }
