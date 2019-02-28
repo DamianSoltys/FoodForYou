b3b97e814707bd37f1814b1f87842189bf12e0dc
macem <marcin.soltysiuk@gmail.com>
2019-02-28 22:50:35 +0100

    Update authentication-guard.guard.ts

--- src/app/authentication-guard.guard.ts
+++ src/app/authentication-guard.guard.ts
@@ -1,50 +1,54 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, CanActivateChild, Router } from '@angular/router';
-import { Observable,of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { LoginService } from './login.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
+
 export class AuthenticationGuardGuard implements CanActivate, CanActivateChild {
-  response;
+  public response: boolean;
+  
   constructor(private login: LoginService, private router: Router) {}
-  canActivate(
+  
+  public canActivate (
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
       if (this.checkauth()) {
-        return true; } else {
+        return true; 
+      } else {
         this.router.navigate(['/']);
         return false;
-        }
+      }
   }
-  canActivateChild(
+  
+  public canActivateChild (
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
       if (this.checkauth()) {
-        return true; } else {
+        return true; 
+      } else {
         this.router.navigate(['/']);
         return false;
-        }
+      }
 
   }
 
-  checkauth() {
+  public checkauth(): Observable {
     this.login.Check_auth().subscribe(res => {
-       if (res['auth'] === 1) {
-         console.log('User');
-         this.response = true;
-
-       } else {
+      if (res['auth'] === 1) {
+        console.log('User');
+        this.response = true;
+      } else {
         console.log('Userblee');
-         this.response = false;
-         window.alert('Prosze sie zalogowac');
-
-       }
+        this.response = false;
+        window.alert('Prosze sie zalogowac');
+      }
     }, error => {
-        console.log('Nie można się zalogować');
-        return this.response = false;
+      console.log('Nie można się zalogować');
+      return this.response = false;
     });
-     return of(this.response);
+    return of(this.response);
   }
 }
